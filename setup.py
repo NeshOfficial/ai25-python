3a996d5771ebfeac30a3038a4ef6dce2b0ffe2a8
faizan6557 <aitisamahmed24@gmail.com>
2024-08-25 11:36:45 +0500

    update the code and file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 import codecs
 
-from setuptools import setup, find_packages
+
 from ai21.version import VERSION
 
 readme_path = os.path.abspath(os.path.dirname(__file__))
@@ -11,16 +11,15 @@ readme_path = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(readme_path, "README.md"), encoding="utf-8") as fh:
     long_description = "\\n" + fh.read()
 
-setup(
-    name="ai21",
+# first(    name="ai21",
     version=VERSION,
     author="AI21 Labs",
     author_email="support@ai21.com",
     long_description_content_type="text/markdown",
     long_description=long_description,
-    packages=find_packages(exclude=["tests", "tests.*"]),
+   
     keywords=["python", "sdk", "ai", "ai21", "jurassic", "ai21-python", "llm"],
     install_requires=[
         "httpx",
     ],
-)
+
--- tasks.py
+++ tasks.py
@@ -1,196 +1,54 @@
-"""
-<<<<<<< refs/remotes/origin/visualization
-tasks to kick-off the project
-=======
-tasks to kick-off the project* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# """
+# <<<<<<< refs/remotes/origin/visualization
+# tasks to kick-off the project
+# =======
+# tasks to kick-off the project* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
 
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
- chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+#  chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
 
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183) chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183) chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
 
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
-
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
-
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
-
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
-
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
-
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
-* chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
-
-* chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
->>>>>>> local
-"""
-
-from invoke import task
-
-
-@task
-def formatter(tsk, fix=False):
-    """
-    python format
-    """
-    auto_fix = "" if fix else "--check --diff"
-    cmd = " && ".join(
-        [
-            f"python -m black . {auto_fix}",
-        ]
-    )
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def lint(tsk, fix=False):
-    """
-    python lint
-    """
-    flags = "--fix" if fix else ""
-    cmd = " && ".join(
-        [
-            f"ruff *.py {flags} ai21/ tests/",
-        ]
-    )
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task(optional=["coverage"], help={"coverage": "[true|false]"})
-def test(tsk, coverage=False):
-    """
-    Run unit tests
-    """
-    cov = "--cov --cov-report=term-missing" if coverage else ""
-    cmd = f"poetry run pytest {cov}"
-    tsk.run(cmd, echo=True, pty=True)
-
-@task
-def audit(tsk):
-    """
-    Run audit check on the dependent packages
-    """
-    cmd = "safety check --full-report"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def staticcheck(tsk):
-    """
-    Run static check on the projects files
-    """
-    cmd = "mypy ai21 tests"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def isort(tsk):
-    """
-    Run static check on the projects files
-    """
-    cmd = "isort ai21 tests"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def audit(tsk):
-    """
-    Run audit check on the dependent packages
-    """
-    cmd = "safety git check --full-report"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def staticcheck(tsk):
-    """
-    Run static check on the projects files
-    """
-    cmd = "mypy ai21 tests"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def isort(tsk):
-    """
-    Run static check on the projects files
-    """
-    cmd = "isort ai21 tests"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def build(tsk):
-    """
-    generate a package for ai21
-    """
-    cmd = "poetry build"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def update(tsk):
-    """
-    update outdated packages
-    """
-    cmd = "poetry update"
-    tsk.run(cmd, echo=True, pty=True)
-
-
-@task
-def outdated(tsk):
-    """
-    update outdated packages
-    """
-    cmd = "poetry show --outdated --top-level"
-    tsk.run(cmd, echo=True, pty=True)
