4c3df42e5d4ec57bbe93ee3360284068631dbe62
M-Aitisam <aitisamahmed24@gmail.com>
2024-08-27 13:46:54 +0500

    update the code

---
+++ storage.py
@@ -0,0 +1,3 @@
+
+# * chore(deps-dev): bump zipp from 3.18.1 to 3.19.1 (#183)
+# * chore(release): v2.9.1 [skip ci] ([`5ed7f07`](https://github.com/AI21Labs/ai21-python/commit/5ed7f07e677ce60eb960991f3c601869d4e2c68c))
